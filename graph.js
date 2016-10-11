ead759ea3204c36b66b904f8f09d2df89d3eea25
cxd4 <cxd4@users.noreply.github.com>
2016-10-11 15:36:53 -0400

    Implement space for 7 more user-defined functions.

--- graph.js
+++ graph.js
@@ -33,6 +33,13 @@ function clear_graph() {
 var inverse_zoom = 1.0; /* graph magnification = 1 / inverse_zoom */
 
 var f_x = "";
+var g_x = "";
+var h_x = "";
+var i_x = "";
+var j_x = "";
+var k_x = "";
+var l_x = "";
+var m_x = "";
 
 function f(x) {
     "use strict";
@@ -43,25 +50,127 @@ function graph_all() {
     "use strict";
     var x = -1.0 / inverse_zoom;
     var i = 0;
+    var raster_pitch = 512;
+    var coords = 2;
+    var stride = 4 * 4; /* coords_per_vertex * sizeof(GLfloat) */
     var vertex_buffer = [];
+
+    while (i < raster_pitch) {
+        vertex_buffer[4*i + X] = x;
+        x += 2.0 / raster_pitch;
+        vertex_buffer[4*i + Z] = 0.0;
+        vertex_buffer[4*i + W] = inverse_zoom;
+        i += 1;
+    }
 
     clear_graph();
     glEnableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
     glLineWidth(1.0);
 
     f_x = document.getElementById("f_x").value;
-    if (f_x) {
-        while (i < 512 && x <= +1.0) {
-            vertex_buffer[4*i + X] = x;
-            vertex_buffer[4*i + Y] = f(x);
-            vertex_buffer[4*i + Z] = 0.0;
-            vertex_buffer[4*i + W] = inverse_zoom;
+    g_x = document.getElementById("g_x").value;
+    h_x = document.getElementById("h_x").value;
+    i_x = document.getElementById("i_x").value;
+    j_x = document.getElementById("j_x").value;
+    k_x = document.getElementById("k_x").value;
+    l_x = document.getElementById("l_x").value;
+    m_x = document.getElementById("m_x").value;
 
-            x += 1.0 / 256;
+    if (f_x) {
+        i = 0;
+        x = -1.0 / inverse_zoom;
+        while (i < raster_pitch) {
+            vertex_buffer[4*i + Y] = f(x);
+            x += 2.0 / raster_pitch;
             i += 1;
         }
-        glColorMask(GL_TRUE, GL_FALSE, GL_FALSE, GL_TRUE);
-        glVertexPointer(4, GL_FLOAT, 0, vertex_buffer);
+        glColor4f(1, 0, 0, 1); /* red (primary) */
+        glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
+        glDrawArrays(GL_LINE_STRIP, 0, i);
+    }
+    if (g_x) {
+        i = 0;
+        x = -1.0 / inverse_zoom;
+        while (i < raster_pitch) {
+            vertex_buffer[4*i + Y] = f(x);
+            x += 2.0 / raster_pitch;
+            i += 1;
+        }
+        glColor4f(0, 0, 1, 1); /* blue (primary) */
+        glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
+        glDrawArrays(GL_LINE_STRIP, 0, i);
+    }
+    if (h_x) {
+        i = 0;
+        x = -1.0 / inverse_zoom;
+        while (i < raster_pitch) {
+            vertex_buffer[4*i + Y] = f(x);
+            x += 2.0 / raster_pitch;
+            i += 1;
+        }
+        glColor4f(1, 1, 0, 1); /* yellow (primary subtractive pigments) */
+        glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
+        glDrawArrays(GL_LINE_STRIP, 0, i);
+    }
+    if (i_x) {
+        i = 0;
+        x = -1.0 / inverse_zoom;
+        while (i < raster_pitch) {
+            vertex_buffer[4*i + Y] = f(x);
+            x += 2.0 / raster_pitch;
+            i += 1;
+        }
+        glColor4f(0, 1, 0, 1); /* green (primary, secondary subtractive) */
+        glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
+        glDrawArrays(GL_LINE_STRIP, 0, i);
+    }
+    if (j_x) {
+        i = 0;
+        x = -1.0 / inverse_zoom;
+        while (i < raster_pitch) {
+            vertex_buffer[4*i + Y] = f(x);
+            x += 2.0 / raster_pitch;
+            i += 1;
+        }
+        glColor4f(1, 0, 1, 1); /* magenta (secondary) */
+        glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
+        glDrawArrays(GL_LINE_STRIP, 0, i);
+    }
+    if (k_x) {
+        i = 0;
+        x = -1.0 / inverse_zoom;
+        while (i < raster_pitch) {
+            vertex_buffer[4*i + Y] = f(x);
+            x += 2.0 / raster_pitch;
+            i += 1;
+        }
+        glColor4f(0, 1, 1, 1); /* cyan (secondary, tertiary subtractive) */
+        glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
+        glDrawArrays(GL_LINE_STRIP, 0, i);
+    }
+    if (l_x) {
+        i = 0;
+        x = -1.0 / inverse_zoom;
+        while (i < raster_pitch) {
+            vertex_buffer[4*i + Y] = f(x);
+            x += 2.0 / raster_pitch;
+            i += 1;
+        }
+        glColor4f(1, 0.5, 0, 1); /* orange (tertiary, secondary subtractive) */
+        glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
+        glDrawArrays(GL_LINE_STRIP, 0, i);
+    }
+    if (m_x) {
+        i = 0;
+        x = -1.0 / inverse_zoom;
+        while (i < raster_pitch) {
+            vertex_buffer[4*i + Y] = f(x);
+            x += 2.0 / raster_pitch;
+            i += 1;
+        }
+        glColor4f(0.5, 0, 1.0, 1); /* indigo (tertiary) */
+        glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
         glDrawArrays(GL_LINE_STRIP, 0, i);
     }
     return;
